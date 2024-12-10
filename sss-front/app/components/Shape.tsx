769d5ab511c8e990042f748b7cad542522a52fe5
valentin-orrit <vorrit@gmail.com>
2024-12-10 12:31:13 +0100

    updated shape comp

--- sss-front/app/components/Shape.tsx
+++ sss-front/app/components/Shape.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+interface ShapeProps {
+    instrument: string
+}
 
-export default function Shape() {
-    const [instrument, setInstrument] = useState('synths')
-
+export default function Shape({ instrument }: ShapeProps) {
     return (
-        <div className="shape">
+        <div className="shape w-20 mx-2">
             <img
                 src={`/shapes/shape_${instrument}.svg`}
-                alt="arps logo"
+                alt={`${instrument} shape`}
                 width="200px"
             />
         </div>
--- sss-front/app/routes/_index.tsx
+++ sss-front/app/routes/_index.tsx
@@ -12,7 +12,12 @@ export default function Index() {
     return (
         <div className="flex h-screen items-center justify-center font-mono text-sssoffwhite  bg-sssdarkblue">
             <div className="flex flex-col items-center">
-                <Shape />
+                <div className="shapes flex m-4">
+                    <Shape instrument="arps" />
+                    <Shape instrument="synths" />
+                    <Shape instrument="pads_logo" />
+                    <Shape instrument="drums_logo" />
+                </div>
                 <h1 className="text-4xl font-normal">
                     super secret <span className="text-sssred">samples</span>
                 </h1>
