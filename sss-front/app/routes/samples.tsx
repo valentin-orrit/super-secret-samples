809ac6fbdea30bd07baa97321ccc9fc09ab2bfe2
valentin-orrit <vorrit@gmail.com>
2025-04-04 10:18:14 +0200

    better responsiveness and style for AudioPlayer(

--- sss-front/app/routes/samples.tsx
+++ sss-front/app/routes/samples.tsx
@@ -77,7 +77,7 @@ export default function SamplesPage() {
                     ))}
                 </div>
                 <div id="empty-margin" className="my-16"></div>
-                <div className="z-100 fixed bottom-0 w-1/2">
+                <div className="z-100 fixed bottom-0 md:w-3/4 xl:w-1/2">
                     <AudioPlayer
                         currentSample={currentSample}
                         isPlaying={isPlaying}
