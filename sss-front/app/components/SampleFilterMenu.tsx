59592d41704789f901eeade937683ee882f4dc53
valentin-orrit <vorrit@gmail.com>
2025-04-11 11:42:39 +0200

    animated dropdown menu chevron

--- sss-front/app/components/SampleFilterMenu.tsx
+++ sss-front/app/components/SampleFilterMenu.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -15,22 +16,26 @@ export default function SamplefilterMenu({
     menuTitle,
     menuContent,
 }: SamplefilterMenuProps) {
+    const [open, setOpen] = useState(false)
+
     return (
-        <DropdownMenu>
+        <DropdownMenu open={open} onOpenChange={setOpen}>
             <DropdownMenuTrigger>
                 <div className="flex bg-white mt-2 px-4 py-2 gap-2 border rounded-xl group">
                     <div className="flex">{menuTitle}</div>
-                    <ChevronDown />
+                    <ChevronDown
+                        className={`transition-transform duration-200 ${
+                            open ? 'rotate-180' : ''
+                        }`}
+                    />
                 </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-                {menuContent.map((menuContentElement) => {
-                    return (
-                        <DropdownMenuItem key={menuContentElement}>
-                            {menuContentElement}
-                        </DropdownMenuItem>
-                    )
-                })}
+                {menuContent.map((menuContentItem) => (
+                    <DropdownMenuItem key={menuContentItem}>
+                        {menuContentItem}
+                    </DropdownMenuItem>
+                ))}
             </DropdownMenuContent>
         </DropdownMenu>
     )
